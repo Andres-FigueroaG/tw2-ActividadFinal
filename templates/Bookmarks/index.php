a991e603450af169e46fc6c8ff21154f876aee76
Andres-FigueroaG <134104347+Andres-FigueroaG@users.noreply.github.com>
2024-05-06 19:37:24 -0400

    cambios en el index del bookmark

--- templates/Bookmarks/index.php
+++ templates/Bookmarks/index.php
@@ -5,18 +5,18 @@
  */
 ?>
 <div class="bookmarks index content">
-    <?= $this->Html->link(__('New Bookmark'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Bookmarks') ?></h3>
+    <?= $this->Html->link(__('new bookmark'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <h3><?= __('bookmarks') ?></h3>
     <div class="table-responsive">
         <table>
             <thead>
                 <tr>
                     <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('user_id') ?></th>
+                    <th><?= $this->Paginator->sort('user') ?></th>
                     <th><?= $this->Paginator->sort('title') ?></th>
                     <th><?= $this->Paginator->sort('created') ?></th>
                     <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
+                    <th class="actions"><?= __('actions') ?></th>
                 </tr>
             </thead>
             <tbody>
@@ -28,9 +28,9 @@
                     <td><?= h($bookmark->created) ?></td>
                     <td><?= h($bookmark->modified) ?></td>
                     <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $bookmark->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $bookmark->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $bookmark->id], ['confirm' => __('Are you sure you want to delete # {0}?', $bookmark->id)]) ?>
+                        <?= $this->Html->link(__('view'), ['action' => 'view', $bookmark->id]) ?>
+                        <?= $this->Html->link(__('edit'), ['action' => 'edit', $bookmark->id]) ?>
+                        <?= $this->Form->postLink(__('delete'), ['action' => 'delete', $bookmark->id], ['confirm' => __('Are you sure you want to delete # {0}?', $bookmark->id)]) ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
@@ -39,12 +39,12 @@
     </div>
     <div class="paginator">
         <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
+            <?= $this->Paginator->first('<< ' . __('primer pagina')) ?>
+            <?= $this->Paginator->prev('< ' . __('anterior')) ?>
             <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
+            <?= $this->Paginator->next(__('siguiente') . ' >') ?>
+            <?= $this->Paginator->last(__('ultima pagina') . ' >>') ?>
         </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+        <p><?= $this->Paginator->counter(__('pagina {{page}} de {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
 </div>
