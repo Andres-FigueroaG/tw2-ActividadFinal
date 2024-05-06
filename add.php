839867b661a5dbe175df102cabd6bc926bd99178
Andres-FigueroaG <134104347+Andres-FigueroaG@users.noreply.github.com>
2024-05-06 19:04:03 -0400

    cambios en el index del proyecto

---
+++ add.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\User $user
+ */
+?>
+<div class="row">
+    <aside class="column">
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="users form content">
+            <?= $this->Form->create($user) ?>
+            <fieldset>
+                <legend><?= __('Registrate') ?></legend>
+                <?php
+                    echo $this->Form->control('email');
+                    echo $this->Form->control('password');
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Registrarse')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,16 +1,50 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @var \App\View\AppView $this
+ * @var iterable<\App\Model\Entity\Bookmark> $bookmarks
  */
-
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+?>
+<div class="bookmarks index content">
+    <?= $this->Html->link(__('new bookmark'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <h3><?= __('bookmarks') ?></h3>
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th><?= $this->Paginator->sort('id') ?></th>
+                    <th><?= $this->Paginator->sort('user') ?></th>
+                    <th><?= $this->Paginator->sort('title') ?></th>
+                    <th><?= $this->Paginator->sort('created') ?></th>
+                    <th><?= $this->Paginator->sort('modified') ?></th>
+                    <th class="actions"><?= __('actions') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($bookmarks as $bookmark): ?>
+                <tr>
+                    <td><?= $this->Number->format($bookmark->id) ?></td>
+                    <td><?= $bookmark->has('user') ? $this->Html->link($bookmark->user->email, ['controller' => 'Users', 'action' => 'view', $bookmark->user->id]) : '' ?></td>
+                    <td><?= h($bookmark->title) ?></td>
+                    <td><?= h($bookmark->created) ?></td>
+                    <td><?= h($bookmark->modified) ?></td>
+                    <td class="actions">
+                        <?= $this->Html->link(__('view'), ['action' => 'view', $bookmark->id]) ?>
+                        <?= $this->Html->link(__('edit'), ['action' => 'edit', $bookmark->id]) ?>
+                        <?= $this->Form->postLink(__('delete'), ['action' => 'delete', $bookmark->id], ['confirm' => __('Are you sure you want to delete # {0}?', $bookmark->id)]) ?>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="paginator">
+        <ul class="pagination">
+            <?= $this->Paginator->first('<< ' . __('primer pagina')) ?>
+            <?= $this->Paginator->prev('< ' . __('anterior')) ?>
+            <?= $this->Paginator->numbers() ?>
+            <?= $this->Paginator->next(__('siguiente') . ' >') ?>
+            <?= $this->Paginator->last(__('ultima pagina') . ' >>') ?>
+        </ul>
+        <p><?= $this->Paginator->counter(__('pagina {{page}} de {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+    </div>
+</div>
